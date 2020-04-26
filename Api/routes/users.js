ce80d23905045beb09891611fd903ec4d4c66b01
Dineshkumar Devaraj <dineshkumar.devaraj@kgisl.com>
2020-04-26 18:00:50 +0530

    Revert "Added an "GET" method for "Routing level Middleware" - 04/26/2020"
    This reverts commit c33a1563c77f186260766a96c0812b48f8d31e36.
    
    Signed-off-by: Dineshkumar Devaraj <dineshkumar.devaraj@kgisl.com>

--- Api/routes/users.js
+++ Api/routes/users.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const router = express.Router();
 const conn = require("../db/db");
-var prettyjson = require("prettyjson");
 router.get("/", (req, res) => {
  var rs = '';
     conn.query("Select * from tbl_lookup ", function(error, result) {
@@ -17,18 +16,5 @@ router.get("/", (req, res) => {
 
     res.send({rs});
 });
-
-
-router.get("/list", (req, res) => {
-    conn.query("select * from tbl_user_master ", function(error, result) {
-      if (error) {
-        res.send(JSON.stringify(error));
-      } else {
-        res.send(prettyjson.render(JSON.stringify(result)));
-      }
-    });
- });
-
-
 
 module.exports = router;
